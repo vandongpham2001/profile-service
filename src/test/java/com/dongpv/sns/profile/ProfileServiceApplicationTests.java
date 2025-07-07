c6256b2df854cf886421d56dbe83f045729f1d05
DongPV <vandongpham2001@gmail.com>
2025-07-07 23:44:44 +0700

    feat: init source

---
+++ src/test/java/com/dongpv/sns/profile/ProfileServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dongpv.sns.profile;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProfileServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
