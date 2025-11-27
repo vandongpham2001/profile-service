9042ed6b6bd24066fbb106ca44d3602d03cfe073
DongPV <vandongpham2001@gmail.com>
2025-11-27 22:30:48 +0700

    feat: enhance project configuration with gRPC support and update dependencies

--- src/main/java/com/dongpv/sns/profile/ProfileServiceApplication.java
+++ src/main/java/com/dongpv/sns/profile/ProfileServiceApplication.java
@@ -2,7 +2,9 @@ package com.dongpv.sns.profile;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableFeignClients
 @SpringBootApplication
 public class ProfileServiceApplication {
 
